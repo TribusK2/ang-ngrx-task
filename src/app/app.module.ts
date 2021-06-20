ad4c7e2a8e1eb6d287ef92161e5c36d2f0040be8
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2021-06-20 08:42:16 +0200

    Init

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import {UserModule} from './user/user.module';
+import {EffectsModule} from '@ngrx/effects';
+import {reducer} from './user/store/users.reducer';
+import {FormsModule} from '@angular/forms';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UserModule,
+    FormsModule,
+    StoreModule.forRoot({ users: reducer }, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
